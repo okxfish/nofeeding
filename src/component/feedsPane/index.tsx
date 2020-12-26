154f4c79b069bc5477244df2027ba86bdf5ba756
lzh <1119548217@qq.com>
2020-12-26 16:49:26 +0800

    temp: 回家;

--- src/component/feedsPane/index.tsx
+++ src/component/feedsPane/index.tsx
@@ -1,10 +1,17 @@
-import * as React from "react";
+import { default as React, useState } from "react";
 import {
+  Text,
+  Image,
+  ImageFit,
+  IconButton,
+  IIconProps,
+  IContextualMenuProps,
+  FontIcon,
+  IImageProps,
   GroupedList,
   IGroupRenderProps,
   IGroupHeaderProps,
-} from "office-ui-fabric-react/lib/GroupedList";
-import { FontIcon } from "office-ui-fabric-react/lib/Icon";
+} from "office-ui-fabric-react";
 import { createGroups } from "@fluentui/example-data";
 
 export interface Props {
@@ -40,8 +47,6 @@ interface Feed {
   sourceID: Source["id"];
   time: string;
   tags: Tag["name"];
-  isRead: string;
-  isStar: string;
 }
 
 interface FeedProps {
@@ -53,10 +58,29 @@ interface FeedProps {
   time?: string;
   isRead?: boolean;
   isStar?: boolean;
+  isPin?: boolean;
+}
+
+interface FeedGroupProps {
+  key: string;
+  id: string;
+  children: FeedGroupProps[];
+  count: number;
+  isCollapsed?: boolean;
+  level: number;
+  name: string;
+  startIndex: number;
 }
 
 const fullSummary: string =
   "this is rss summary at next but not last, you need to do a lot of things to make the world in better place. as people say, time is gold, nobody can reject the air.";
+
+interface GroupData {
+  key: string;
+  index: string;
+  name: string;
+  children: FeedProps[];
+}
 
 const createItems = (count: number): FeedProps[] => {
   return Array.from({
@@ -66,57 +90,203 @@ const createItems = (count: number): FeedProps[] => {
     const rdNumber: number = Math.floor(Math.random() * rdBase);
     const rdBool: boolean = rdNumber / rdBase > 0.5;
     return {
-      key: index,
-      id: index,
-      title: `this is rss source: ${index}`,
+      key: rdNumber,
+      id: rdNumber,
+      title: `this is rss source: ${rdNumber}`,
       summary: fullSummary.slice(rdNumber),
       sourceName: "some news",
       time: "3 days",
       isRead: rdBool,
       isStar: rdBool,
+      isPin: rdBool,
     };
   });
 };
 
-const items: FeedProps[] = createItems(Math.pow(groupCount, groupDepth + 1));
+const createGroupsData = (count: number): GroupData[] => {
+  return Array.from({
+    length: count,
+  }).map((item: any, index: number): any => {
+    return {
+      key: index,
+      id: index,
+      name: `group (${index})`,
+      children: createItems(3),
+    };
+  });
+};
 
-const groups = createGroups(groupCount, groupDepth, 0, groupCount);
+const groupData: GroupData[] = createGroupsData(3);
+
+const transItemsProps = (data: GroupData[]): FeedProps[] => {
+  return data
+    .map((item: GroupData, index: number) =>
+      item.children.map((feed) => ({ ...feed, groupIndex: index }))
+    )
+    .flat();
+};
+
+const transGroupProps = (data: GroupData[]): FeedGroupProps[] => {
+  return data.reduce(
+    (
+      previousValue: FeedGroupProps[],
+      currentValue: GroupData,
+      currentIndex: number
+    ): FeedGroupProps[] => {
+      const rdBase: number = 100;
+      const rdNumber: number = Math.floor(Math.random() * rdBase);
+      const group = {
+        key: String(rdNumber),
+        id: String(rdNumber),
+        count: currentValue.children.length,
+        isCollapsed: false,
+        level: 0,
+        name: `Group ${rdNumber}`,
+        children: [],
+        startIndex: 0,
+      };
+      previousValue.push(group);
+      return previousValue;
+    },
+    []
+  );
+};
+
 export interface Props {
   className?: string;
 }
 
 const FeedsPane = ({ className }: Props) => {
+  const [data, setData] = useState<any>(groupData);
+  const items = transItemsProps(data);
+  const groups = transGroupProps(data);
+
   const onRenderCell = (
     nestingDepth?: number,
     item?: FeedProps,
     itemIndex?: number
   ): React.ReactNode => {
+    const imageProps: IImageProps = {
+      src: "http://placehold.it/100x75",
+      width: 100,
+      height: 100,
+      imageFit: ImageFit.cover,
+    };
+
+    const moreIcon: IIconProps = { iconName: "More" };
+    const favoriteStarIcon: IIconProps = { iconName: "FavoriteStar" };
+    const favoriteStarFillIcon: IIconProps = { iconName: "FavoriteStarFill" };
+    const pinSolid12Icon: IIconProps = { iconName: "PinSolid12" };
+    const pinSolidOff12Icon: IIconProps = { iconName: "PinSolidOff12" };
+    const readingModeIcon: IIconProps = { iconName: "ReadingMode" };
+    const readingModeSolidIcon: IIconProps = { iconName: "ReadingModeSolid" };
+    const menuProps: IContextualMenuProps = {
+      items: [
+        {
+          key: "emailMessage",
+          text: "Email message",
+          iconProps: { iconName: "Mail" },
+        },
+        {
+          key: "calendarEvent",
+          text: "Calendar event",
+          iconProps: { iconName: "Calendar" },
+        },
+      ],
+      directionalHintFixed: true,
+    };
+
+    const updateFieldInItem = (field: string): void => {
+      if (!item || typeof item.groupIndex === "undefined") {
+        return;
+      } else {
+        const newData: GroupData[] = [
+          ...items.slice(0, itemIndex),
+          {
+            ...item,
+            [field]: !item[field],
+          },
+          ...items.slice(itemIndex + 1),
+        ];
+        setData(newData);
+      }
+    };
+
+    const handleMarkAsReadClick = (field: string): void => {
+      updateFieldInItem(field);
+    };
+
     return item && typeof itemIndex === "number" && itemIndex > -1 ? (
-      <div className="cursor-pointer border-b border-gray-300 mb-4 pb-4">
-        <div className="text-lg text-gray-800 font-medium">{item.title}</div>
-        <div className="text-base text-gray-600 break-all">{item.summary}</div>
+      <div className="flex border-b border-gray-300 mb-4 pb-4 cursor-pointer group">
+        <Image className="flex-shrink-0 mr-3" {...imageProps} />
+        <div className="flex-1">
+          <div className="relative flex items-start mb-2 text-lg text-gray-800 leading-none font-medium">
+            <span className="flex-1">{item.title}</span>
+          </div>
+          <div className="text-base text-gray-600">{item.summary}</div>
+          <div className="flex items-center justify-end w-full">
+            <div className="flex-1 text-sm text-gray-400">
+              <span className="pr-1">{item.sourceName}</span>
+              <span>/</span>
+              <span className="pl-1">{item.time}</span>
+            </div>
+            <IconButton
+              className="focus:outline-none"
+              iconProps={item.isPin ? pinSolid12Icon : pinSolidOff12Icon}
+              title="pin as unread"
+              ariaLabel="Pin as unread"
+              disabled={false}
+              onClick={handleMarkAsReadClick.bind(null, "isPin")}
+            />
+            <IconButton
+              className="focus:outline-none"
+              iconProps={item.isStar ? favoriteStarFillIcon : favoriteStarIcon}
+              title="favorite"
+              ariaLabel="Favorite"
+              disabled={false}
+              onClick={handleMarkAsReadClick.bind(null, "isStar")}
+            />
+            <IconButton
+              className="focus:outline-none"
+              iconProps={item.isRead ? readingModeSolidIcon : readingModeIcon}
+              title="mark as read"
+              ariaLabel="Mark as read"
+              disabled={false}
+              onClick={handleMarkAsReadClick.bind(null, "isRead")}
+            />
+            <IconButton
+              className="focus:outline-none"
+              menuProps={menuProps}
+              iconProps={moreIcon}
+              onRenderMenuIcon={() => null}
+              title="more"
+              ariaLabel="More"
+              disabled={false}
+              checked={false}
+            />
+          </div>
+        </div>
       </div>
     ) : null;
   };
 
+  const onRenderHeader = (props?: IGroupHeaderProps): JSX.Element | null => {
+    if (props && props.group) {
+      return (
+        <div
+          className="cursor-pointer text-gray-600 text-lg font-bold leading-loose border-b border-gray-600 mb-4"
+          // onClick={toggleCollapse}
+        >
+          {props.group!.name}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
+
   const groupProps: IGroupRenderProps = {
-    onRenderHeader: (props?: IGroupHeaderProps): JSX.Element | null => {
-      if (props && props.group) {
-        const toggleCollapse = (): void => {
-          props.onToggleCollapse!(props.group!);
-        };
-        return (
-          <div
-            className="cursor-pointer text-gray-600 text-lg font-bold leading-loose border-b border-gray-600 mb-4"
-            onClick={toggleCollapse}
-          >
-            {props.group!.name}
-          </div>
-        );
-      } else {
-        return null;
-      }
-    },
+    onRenderHeader: onRenderHeader,
   };
 
   return (
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -6,13 +6,13 @@ import { default as ArticlePane } from "../../component/articlePane";
 const Home = () => {
   return (
     <div className="flex h-screen">
-      <div className="w-2/12">
+      <div className="xl:w-2/12">
         <OverviewPane />
       </div>
-      <div className="scrollbar w-3/12 bg-gray-100 h-full overflow-x-auto">
+      <div className="scrollbar xl:w-3/12 lg:w-4/12 bg-gray-100 h-full overflow-x-auto">
         <FeedsPane />
       </div>
-      <div className="w-7/12 bg-white">
+      <div className="xl:w-7/12 lg:w-8/12 bg-white">
         <ArticlePane />
       </div>
     </div>
