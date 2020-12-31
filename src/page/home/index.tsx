8e3fd00a1e16f850b01a7738de313f2b89ebfde2
lzh <1119548217@qq.com>
2020-12-31 13:33:29 +0800

    feat: 增加响应布局，完善侧边栏，增加分栏视图文章;

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,36 +1,115 @@
-import React, { useState } from "react";
+import React, { ReactFragment, useState } from "react";
 import { default as OverviewPane } from "../../component/overviewPane";
 import { default as FeedsPane } from "../../component/feedsPane";
 import { default as ArticlePane } from "../../component/articlePane";
-import { Modal } from "office-ui-fabric-react";
+import { Modal, IconButton, IIconProps, Text } from "office-ui-fabric-react";
 import feedsMockData from "../../mock/feed";
+
+const globalNavButtonIcon: IIconProps = { iconName: "GlobalNavButton" };
+const addIcon: IIconProps = { iconName: "Add" };
+const searchIcon: IIconProps = { iconName: "Search" };
+const syncIcon: IIconProps = {
+  iconName: "Sync",
+  style: { transform: "rolate" },
+};
+const settingsIcon: IIconProps = { iconName: "Settings" };
+
+interface SidePaneItemProps {
+  icon?: IIconProps;
+  text?: string;
+  className?: string;
+  isGap?: boolean;
+  onClick?(e: any): void;
+}
 
 const Home = () => {
   const [isArticleModalOpen, setIsArticleModalOpen] = useState<boolean>(false);
+  const [isSidePaneOpen, setIsSidePaneOpen] = useState<boolean>(true);
   const hideModal = (): void => setIsArticleModalOpen(false);
   const openModal = (): void => setIsArticleModalOpen(true);
+  const toggleSidePane = (): void => setIsSidePaneOpen(!isSidePaneOpen);
+
+  const sidePaneItems: SidePaneItemProps[] = [
+    {
+      icon: globalNavButtonIcon,
+      text: "menu",
+      onClick: toggleSidePane,
+    },
+    {
+      icon: searchIcon,
+      text: "search",
+      onClick: () => console.log("search"),
+    },
+    {
+      icon: addIcon,
+      text: "add",
+      onClick: () => console.log("add"),
+    },
+    {
+      isGap: true,
+    },
+    {
+      icon: syncIcon,
+      text: "sync",
+      onClick: () => console.log("sync"),
+    },
+    {
+      icon: settingsIcon,
+      text: "settings",
+      onClick: () => console.log("settings"),
+    },
+  ];
+
+  const sidePeneRender = (): ReactFragment => {
+    return sidePaneItems.map((item: SidePaneItemProps) => {
+      if (item.isGap) {
+        return <div className="flex-1"></div>;
+      } else {
+        return (
+          <div className={`w-full h-10 flex items-center ${item?.className}`}>
+            <IconButton
+              className="w-full h-full"
+              iconProps={item?.icon}
+              onClick={item?.onClick}
+            >
+              {isSidePaneOpen && (
+                <Text className="flex-1 text-left ml-2">{item?.text}</Text>
+              )}
+            </IconButton>
+          </div>
+        );
+      }
+    });
+  };
 
   return (
     <div className="flex h-screen">
-      <div className="flex flex-col w-16 bg-gray-300">
-        
+      <div
+        className={`w-${
+          isSidePaneOpen ? "48" : "16"
+        } flex flex-col items-center bg-gray-300 transition-all`}
+      >
+        {sidePeneRender()}
       </div>
-      <div className="w-80 bg-gray-100">
+      <div className="w-80 bg-gray-200">
         <OverviewPane />
       </div>
-      <div className="flex-1 bg-white">
+      <div className="bg-white">
         <FeedsPane
-          className="scrollbar h-full overflow-x-auto bg-gray-100 mx-auto lg:w-160"
+          className="scrollbar h-full overflow-y-auto bg-gray-50 lg:w-auto xl:w-112 2xl:w-128 transition-all"
           onClickFeed={openModal}
         />
+      </div>
+      <div className="lg:hidden xl:block flex-1 h-full scrollbar overflow-y-auto">
+        <ArticlePane className="px-6" />
       </div>
       <Modal
         isOpen={isArticleModalOpen}
         onDismiss={hideModal}
-        overlay={{style: {backdropFilter: "blur(16px)"}}}
+        overlay={{ style: { backdropFilter: "blur(16px)" } }}
         isBlocking={false}
       >
-        <ArticlePane className="px-6" style={{maxHeight: '95vh'}}/>
+        <ArticlePane className="px-6" style={{ maxHeight: "95vh" }} />
       </Modal>
     </div>
   );
