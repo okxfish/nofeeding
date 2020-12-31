8d35ab48cc1775de12b5b9f14a66cdcd8df2b7b4
lzh <1119548217@qq.com>
2020-12-31 10:33:12 +0800

    feat: 增加自定义的 tailwind 配置，增加更多的 spacing 长度;

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '160': '40rem',
+        '176': '48rem',
+        '192': '56rem',
+      }
+    },
   },
   variants: {
     extend: {},
