3562c0bfae0767f8b7f404cb32c091a3db95d120
lzh <1119548217@qq.com>
2021-05-16 13:25:47 +0800

    feat: 修改接口调用的方法;

--- src/api/inoreader.ts
+++ src/api/inoreader.ts
@@ -16,7 +16,7 @@ export const inoreader = {
   getStreamContents: () =>
     fetch.get(`/reader/api/0/stream/contents`, {
       params: {
-        n: "", // Number of items to return (default 20, max 1000).
+        n: 100, // Number of items to return (default 20, max 1000).
         r: "", // Order. By default, it is newest first. You can pass o here to get oldest first.
         ot: "", // Start time (unix timestamp) from which to start to get items. If r=o and the time is older than one month ago, one month ago will be used instead.
         xt: "", // Exclude Target - You can query all items from a feed that are not flagged as read by setting this to user/-/state/com.google/read.
@@ -39,7 +39,7 @@ export const inoreader = {
     }),
   // Folder/Tag list
   getFolderOrTagList: (types: string, counts: string) =>
-    fetch.post(`/reader/api/0/tag/list`, null, {
+    fetch.get(`/reader/api/0/tag/list`, {
       params: {
         types: types,
         counts: counts,
