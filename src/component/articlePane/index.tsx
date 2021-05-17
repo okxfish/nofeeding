fa91c092828e00d163beb42016c9ab70b573181a
lzh <1119548217@qq.com>
2021-05-17 18:15:26 +0800

    feat: 增加 ArticlePane 组件的 ref;

--- src/component/articlePane/index.tsx
+++ src/component/articlePane/index.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
 import { IconButton, IIconProps } from "office-ui-fabric-react";
 import { Parser as HtmlToReactParser } from "html-to-react";
 import "./style.css";
@@ -19,52 +25,60 @@ export interface Props {
 
 const backIcon: IIconProps = { iconName: "Back" };
 
-const ArticlePane = ({
-  className,
-  style,
-  article = { title: "", content: "" },
-  closeModal,
-}: Props) => {
-  const htmlToReactParserRef = useRef(new HtmlToReactParser());
-  const [contentJSX, setContentJSX] = useState<JSX.Element | null>(null);
+const ArticlePane = forwardRef(
+  (
+    {
+      className,
+      style,
+      article = { title: "", content: "" },
+      closeModal,
+    }: Props,
+    ref
+  ) => {
+    const htmlToReactParserRef = useRef(new HtmlToReactParser());
+    const [contentJSX, setContentJSX] = useState<JSX.Element | null>(null);
+    const rootNodeRef = useRef<any>(null);
 
-  useEffect(() => {
-    const parse = htmlToReactParserRef.current.parse;
-    setContentJSX(parse(article.content));
-  }, [article.content]);
+    useImperativeHandle(ref, () => rootNodeRef.current);
 
-  const contentRender = () => {
+    useEffect(() => {
+      const parse = htmlToReactParserRef.current.parse;
+      setContentJSX(parse(article.content));
+    }, [article.content]);
+
+    const contentRender = () => {
+      return (
+        <div className="flex flex-col h-full overflow-y-hidden">
+          <div className="flex items-center h-10 border-b mx-6">
+            <IconButton
+              className="block lg:hidden"
+              iconProps={backIcon}
+              onClick={closeModal}
+            />
+          </div>
+          <div className="article-wrapper overflow-y-scroll scrollbar flex-1 px-6">
+            <article className="max-w-3xl w-full mx-auto py-4">
+              <header>
+                <h2 className="font-bold text-3xl mb-6 break-words">
+                  <a href={article.url} target="_blank" rel="noreferrer">
+                    {article.title}
+                  </a>
+                </h2>
+              </header>
+              <div>{contentJSX}</div>
+              <footer></footer>
+            </article>
+          </div>
+        </div>
+      );
+    };
+
     return (
-      <div className="flex flex-col h-full overflow-y-hidden">
-        <div className="flex items-center h-10 border-b mx-6">
-          <IconButton
-            className="block lg:hidden"
-            iconProps={backIcon}
-            onClick={closeModal}
-          />
-        </div>
-        <div className="article-wrapper overflow-y-scroll scrollbar flex-1 px-6">
-          <article className="max-w-3xl w-full mx-auto py-4">
-            <header>
-              <h2 className="font-bold text-3xl mb-6 break-words">
-                <a href={article.url} target="_blank" rel="noreferrer">
-                  {article.title}
-                </a>
-              </h2>
-            </header>
-            <div>{contentJSX}</div>
-            <footer></footer>
-          </article>
-        </div>
+      <div className={`${className}`} style={style} ref={rootNodeRef}>
+        {contentRender()}
       </div>
     );
-  };
-
-  return (
-    <div className={`${className}`} style={style}>
-      {contentRender()}
-    </div>
-  );
-};
+  }
+);
 
 export default ArticlePane;
