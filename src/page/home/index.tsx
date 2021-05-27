8d846e37f3e2daee5830ea6ac7b2863913a6231e
lzh <1119548217@qq.com>
2021-05-27 16:51:17 +0800

    refactor: delete unused code;

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -2,13 +2,12 @@ import React, { useState } from "react";
 import { default as FeedPage } from "../feed";
 import { useWindowSize } from "react-use";
 import { Route, Switch } from "react-router-dom";
-import "./style.css";
 import SideBar from "./sideBar";
+import "./style.css";
 
 const Home = () => {
   const { height: windowHeight } = useWindowSize();
   const [isOverViewPaneOpen, setIsOverViewPaneOpen] = useState<boolean>(false);
-  const emptyRender = (): React.ReactElement | null => null;
 
   return (
     <div
