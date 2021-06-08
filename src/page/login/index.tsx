b4b62963760dd29b9faaf5a10b3631f07ae44548
lzh <1119548217@qq.com>
2021-06-08 17:33:19 +0800

    feat: add log in indicator in login page;

--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -5,25 +5,26 @@ import {
   PrimaryButton,
   DefaultButton,
   Separator,
+  Spinner,
+  SpinnerSize,
 } from "@fluentui/react";
 import "./style.css";
 import { useInoreaderToken } from "./../../utils/useInoreaderToken";
 import { useHistory } from "react-router-dom";
 
 const Login = () => {
-  const [isLoginWithInoreader, setIsLoginWithInoreader] = useState<boolean>(
-    false
-  );
+  const [isLoginWithInoreader, setIsLoginWithInoreader] =
+    useState<boolean>(false);
 
   const history = useHistory();
   const inoreaderToken = useInoreaderToken();
 
   useEffect(() => {
-    if (isLoginWithInoreader && inoreaderToken) {
+    if (inoreaderToken) {
       setIsLoginWithInoreader(false);
       history.push("/");
     }
-  }, [inoreaderToken, isLoginWithInoreader, history]);
+  }, [inoreaderToken, history]);
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
@@ -44,31 +45,41 @@ const Login = () => {
     <div className="login-page">
       <header className="h-48"></header>
       <div className="max-w-2xl mx-auto mt-8 px-4">
-        <form className="" onSubmit={handleOnSubmit}>
-          <TextField
-            className="mb-2"
-            label="Account"
-            name="account"
-            type="email"
-            required
-            placeholder="your emaill here"
-          />
-          <TextField
-            className="mb-4"
-            label="Password"
-            name="password"
-            type="password"
-            required
-            placeholder="you password here"
-          />
-          <PrimaryButton className="block w-full mt-6" type="submit">
-            log in
-          </PrimaryButton>
-        </form>
-        <Separator className="mt-8 mb-4" alignContent="center">log in with</Separator>
-        <div>
-          <DefaultButton onClick={loginWithInoreader}>inoreader</DefaultButton>
-        </div>
+        {isLoginWithInoreader ? (
+          <Spinner label="log in ···" size={SpinnerSize.large} styles={{circle: 'w-16 h-16 border-8', label: 'text-2xl'}}/>
+        ) : (
+          <>
+            <form className="" onSubmit={handleOnSubmit}>
+              <TextField
+                className="mb-2"
+                label="Account"
+                name="account"
+                type="email"
+                required
+                placeholder="your emaill here"
+              />
+              <TextField
+                className="mb-4"
+                label="Password"
+                name="password"
+                type="password"
+                required
+                placeholder="you password here"
+              />
+              <PrimaryButton className="block w-full mt-6" type="submit">
+                log in
+              </PrimaryButton>
+            </form>
+            <Separator className="mt-8 mb-4" alignContent="center">
+              log in with
+            </Separator>
+            <div>
+              <DefaultButton onClick={loginWithInoreader}>
+                inoreader
+              </DefaultButton>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
