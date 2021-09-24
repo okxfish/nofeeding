d50db8f85ae9d566a9741c6245ba788dedca33a6
zune <1119548217@qq.com>
2021-09-24 12:51:24 +0800

    feat: 增加版本号;

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nofeeding-0.2.8'
+const CACHE_NAME = 'nofeeding-0.2.9'
 const contentToCache = ['/index.html', '/static/', '/images/']
 
 const self = this
--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -109,7 +109,7 @@ const Login = () => {
                                 {t("enter mock mode")}
                             </DefaultButton>  
                         </Stack>
-                        <Text className="text-sm">{t('version')}: 0.2.8</Text>
+                        <Text className="text-sm">{t('version')}: 0.2.9</Text>
                     </Stack>
                 </div>
                 {/* <Stack.Item
